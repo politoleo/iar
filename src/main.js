6a9901fb7fc7fe635848a81f658463f0a2aee9b5
politoleo <politoleo@hotmail.it>
2017-11-27 09:28:46 +0100

    Support root variables

--- src/main.js
+++ src/main.js
@@ -3,20 +3,27 @@ const fs = require('fs');
 const Iar = require('./iar');
 
 var iar = undefined;
+var folder = undefined;
+
+function resolve(string){
+    var out =  string.split("${workspaceRoot}").join(folder);
+    out =  out.split("${workspaceFolder}").join(folder);
+    return out;
+}
 
 function activate(context) {
 
     if (!vscode.workspace.rootPath)
         return;
 
-    var folder = vscode.workspace.rootPath;
+    folder = vscode.workspace.rootPath;
     config_file = folder + "/.vscode/iar.json";
 
     let disposable = vscode.commands.registerCommand('iar.build', function () {
         if (fs.existsSync(config_file)) {
             var obj = JSON.parse(fs.readFileSync(config_file));
             if (obj["config"] && obj["path"] && obj["project"]) {
-                iar = new Iar(obj.path, obj.project, obj.config, folder);
+                iar = new Iar(resolve(obj.path), resolve(obj.project), obj.config, folder);
                 iar.build();
             }
         }
