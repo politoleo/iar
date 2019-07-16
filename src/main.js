d965102a1b8e972a39cf1731995712e845cf6044
Leonardo Polito <leonardo.polito@texa.com>
2019-07-16 11:23:03 +0200

    Add iar.json saved event

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ const vscode = require('vscode');
 const fs = require('fs');
 const Iar = require('./iar');
 
-var iar = undefined;
-var folder = undefined;
+iar = undefined;
+folder = undefined;
 
 function resolve(string) {
     var out = string.split("${workspaceRoot}").join(folder);
@@ -17,13 +17,22 @@ function activate(context) {
         return;
 
     folder = vscode.workspace.rootPath;
-    config_file = folder + "/.vscode/iar.json";
+    config_file = folder + "\\.vscode\\iar.json";
+
+    vscode.workspace.onDidSaveTextDocument((e) => 
+    {
+        if(config_file === e.fileName)
+        {
+            if (!(typeof iar === 'undefined' || iar === null)) 
+                delete iar
+        }
+    })
 
     let disposable = vscode.commands.registerCommand('iar.build', function () {
         if (fs.existsSync(config_file)) {
             var obj = JSON.parse(fs.readFileSync(config_file));
             if (obj["config"] && obj["path"] && obj["project"]) {
-                if(!iar)
+                if (typeof iar === 'undefined' || iar === null) 
                     iar = new Iar(resolve(obj.path), resolve(obj.project), obj.config, folder);
                 if(iar.in_progress() == false)
                     iar.build();
